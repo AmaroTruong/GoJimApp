06223cfe821e9f7191549ebcac9fcf300e29d0bd
Jackie Cheng <jc51887@gmail.com>
2023-06-23 22:23:41 -0400

    Views before Firebase

--- TestingGoJim/FirstView.swift
+++ TestingGoJim/FirstView.swift
@@ -1,23 +1,27 @@
+//
+//  SwiftUIView.swift
+//  GoJim
+//
+//  Created by Jackie Cheng on 6/22/23.
+//
+
 import SwiftUI
 
-struct ContentView: View {
+struct FirstView: View {
     var body: some View {
         NavigationView {
             ZStack
             {
-                    Text("Welcome to GoJim!")
-                        .font(.title)
-                        .position(x: 200, y: 30)
-                    Text("Our mission is to provide affordable access to fitness-tracking instruments and dietary suggestions for all gymgoers, regardless of their expertise, in hopes to encourage the development of a healthier lifestyle!")
-                        .font(.system(size:24))
+                    Text("2023 just started. What a great time to start working out, but how do you start? Personal trainers and fitness apps are all paywalled and complicated. We would like to introduce GoJim!, a fitness-tracking software to track calories, log your exercises, and communicate with other gym goers.")
+                        .font(.system(size:25))
                         .frame(width: 300, height: 500)
-                        .position(x: 200, y: 200)
+                        .position(x: 200, y: 250)
                 Rectangle()
                     .fill(.red)
                     .frame(width: 1200, height: 400)
                     .position(x: 110 , y: 100)
                     .rotationEffect(Angle(degrees: 150))
-                NavigationLink(destination: FirstView().navigationBarBackButtonHidden(true)) {
+                NavigationLink(destination: SecondView().navigationBarBackButtonHidden(true)) {
                     Text("Continue")
                         .foregroundColor(.white)
                         .frame(width: 200, height: 40)
@@ -29,9 +33,10 @@ struct ContentView: View {
             }
         }
     }
-    struct ContentView_Previews: PreviewProvider {
+    
+    struct FirstView_Previews: PreviewProvider {
         static var previews: some View {
-            ContentView()
+            FirstView()
         }
     }
 }
---
+++ TestingGoJim/SecondView.swift
@@ -0,0 +1,51 @@
+//
+//  ThirdView.swift
+//  GoJim
+//
+//  Created by Jackie Cheng on 6/22/23.
+//
+
+import SwiftUI
+
+struct SecondView: View {
+    var body: some View {
+        NavigationView {
+            ZStack
+            {
+                Rectangle()
+                    .fill(.red)
+                    .frame(width: 1200, height: 400)
+                    .position(x: 0, y: -100)
+                    .rotationEffect(Angle(degrees: 325))
+                Rectangle()
+                    .fill(.red)
+                    .frame(width: 1200, height: 400)
+                    .position(x: 110, y: 100)
+                    .rotationEffect(Angle(degrees: 150))
+                            Text("Start the new year off right!")
+                                .font(.system(size:55))
+                                .frame(width: 300, height: 500)
+                                .position(x: 200, y: 230)
+                            Text("GoJim!")
+                                .font(.system(size:70))
+                                .frame(width: 300, height: 500)
+                                .position(x: 180, y: 380)
+                NavigationLink(destination: FirstView().navigationBarBackButtonHidden(true)) {
+                    Text("Get Started!")
+                        .foregroundColor(.white)
+                        .frame(width: 200, height: 40)
+                        .background(.black)
+                        .cornerRadius(15)
+                        .padding()
+                }.frame(maxHeight: .infinity, alignment: .bottom)
+                
+            }
+        }
+    }
+    
+    struct SecondView_Previews: PreviewProvider {
+        static var previews: some View {
+            SecondView()
+        }
+    }
+}
