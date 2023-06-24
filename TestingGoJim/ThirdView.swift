728fc997e2759f644001e55e829af7b09632b5a6
Jackie Cheng <jc51887@gmail.com>
2023-06-24 00:11:24 -0400

    Register Function for Login

--- TestingGoJim/ThirdView.swift
+++ TestingGoJim/ThirdView.swift
@@ -5,7 +5,7 @@
 //  Created by Jackie Cheng on 6/23/23.
 
 import SwiftUI
-import FirebaseCore
+import Firebase
 
 class AppDelegate: NSObject, UIApplicationDelegate {
   func application(_ application: UIApplication,
@@ -21,61 +21,71 @@ struct ThirdView: View {
     @State private var password: String = ""
     var body: some View {
         NavigationView {
-                    ZStack {
-                        VStack {
-                            HStack {
-                                Text("Already Have An Account?")
-                            }
-                            HStack {
-                                Image(systemName: "person")
-                                TextField("Username", text: $username)
-                            }.frame(width: 400, height: 10)
-                            HStack {
-                                SecureField("Password", text: $password)
-                            }.frame(width: 345, height: 10)
-                            NavigationLink {
-                                WelcomePage()
-                            } label: {
-                                Text("Login")
-                                    .foregroundColor(.white)
-                                    .frame(width: 200, height: 40)
-                                    .background(.black)
-                                    .cornerRadius(15)
-                                    .padding()
-                            }
-                            Text("Join us!")
-                                .position(x: 215, y: 600)
-                            NavigationLink {
-                                CreateAccountPage()
-                            } label: {
-                                Text("Create Account")
-                                    .foregroundColor(.white)
-                                    .frame(width: 200, height: 40)
-                                    .background(.black)
-                                    .cornerRadius(15)
-                                    .padding()
-                            }
-                        }
+            ZStack {
+                VStack {
+                    HStack {
+                        Text("Already Have An Account?")
                     }
+                    HStack {
+                        Image(systemName: "person")
+                        TextField("Username", text: $username)
+                    }.frame(width: 400, height: 10)
+                    HStack {
+                        SecureField("Password", text: $password)
+                    }.frame(width: 345, height: 10)
+                    NavigationLink {
+                        WelcomePage()
+                    } label: {
+                        Text("Login")
+                            .foregroundColor(.white)
+                            .frame(width: 200, height: 40)
+                            .background(.black)
+                            .cornerRadius(15)
+                            .padding()
+                    }
+                    Text("Join us!")
+                        .position(x: 215, y: 600)
+                    NavigationLink {
+                        CreateAccountPage()
+                    } label: {
+                        Text("Create Account")
+                            .foregroundColor(.white)
+                            .frame(width: 200, height: 40)
+                            .background(.black)
+                            .cornerRadius(15)
+                            .padding()
+                    }
+                    
                 }
             }
         }
-
-struct ThirdView_Previews: PreviewProvider {
-    static var previews: some View {
-        ThirdView()
     }
-}
-
-struct YourApp: App {
-  @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
-
-
-  var body: some Scene {
-    WindowGroup {
-      NavigationView {
-        ThirdView()
-      }
+    func register() {
+        Auth.auth().createUser(withEmail: username, password: password) { authResult, error in
+            if error != nil {
+                print(error!.localizedDescription)
+            }
+        }
     }
-  }
+    
+    struct ThirdView_Previews: PreviewProvider {
+        static var previews: some View {
+            ThirdView()
+        }
+    }
+    
+    struct ThirdViewApp: App {
+        
+        init() {
+            FirebaseApp.configure()
+        }
+        
+        var body: some Scene {
+            WindowGroup {
+                NavigationView {
+                    ThirdView()
+                }
+            }
+        }
+    }
 }
