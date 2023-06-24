838c0fc096cf67c0e1387e55f348baadbef914c8
Jackie Cheng <jc51887@gmail.com>
2023-06-23 22:07:33 -0400

    Initial Commit

---
+++ TestingGoJim/TestingGoJimApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestingGoJimApp.swift
+//  TestingGoJim
+//
+//  Created by Jackie Cheng on 6/23/23.
+//
+
+import SwiftUI
+
+@main
+struct TestingGoJimApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
