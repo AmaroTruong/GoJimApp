67d3beb22ef9146020c3f280bd3a9acca2eff9ac
Jackie Cheng <jc51887@gmail.com>
2023-06-23 23:26:22 -0400

    Login Page before Firebase

---
+++ TestingGoJim/CreateAccountPage.swift
@@ -0,0 +1,20 @@
+//
+//  CreateAccountPage.swift
+//  TestingGoJim
+//
+//  Created by Jackie Cheng on 6/23/23.
+//
+
+import SwiftUI
+
+struct CreateAccountPage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CreateAccountPage_Previews: PreviewProvider {
+    static var previews: some View {
+        CreateAccountPage()
+    }
+}
--- TestingGoJim/ThirdView.swift
+++ TestingGoJim/ThirdView.swift
@@ -3,15 +3,52 @@
 //  TestingGoJim
 //
 //  Created by Jackie Cheng on 6/23/23.
-//
-
 import SwiftUI
 
 struct ThirdView: View {
+    @State private var username: String = ""
+    @State private var password: String = ""
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
-}
+        NavigationView {
+                    ZStack {
+                        VStack {
+                            HStack {
+                                Text("Already Have An Account?")
+                            }
+                            HStack {
+                                Image(systemName: "person")
+                                TextField("Username", text: $username)
+                            }.frame(width: 400, height: 10)
+                            HStack {
+                                SecureField("Password", text: $password)
+                            }.frame(width: 345, height: 10)
+                            NavigationLink {
+                                WelcomePage()
+                            } label: {
+                                Text("Login")
+                                    .foregroundColor(.white)
+                                    .frame(width: 200, height: 40)
+                                    .background(.black)
+                                    .cornerRadius(15)
+                                    .padding()
+                            }
+                            Text("Join us!")
+                                .position(x: 215, y: 600)
+                            NavigationLink {
+                                CreateAccountPage()
+                            } label: {
+                                Text("Login")
+                                    .foregroundColor(.white)
+                                    .frame(width: 200, height: 40)
+                                    .background(.black)
+                                    .cornerRadius(15)
+                                    .padding()
+                            }
+                        }
+                    }
+                }
+            }
+        }
 
 struct ThirdView_Previews: PreviewProvider {
     static var previews: some View {
