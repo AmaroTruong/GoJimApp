139d11a9512bcdea1845d82c1338f1ec58eb00ca
Jackie Cheng <jc51887@gmail.com>
2023-06-24 14:09:50 -0400

    Random quote integration API

--- TestingGoJim/ThirdView.swift
+++ TestingGoJim/ThirdView.swift
@@ -1,31 +1,35 @@
 import SwiftUI
-import Foundation
 
 struct ThirdView: View {
     @State private var quote: String = ""
+    
     var body: some View {
         NavigationView {
             ZStack {
                 Color("LightRed")
                     .ignoresSafeArea()
-                VStack {
+                
+                VStack(spacing: 0) {
                     Text(quote)
-                        .foregroundColor(.white)
+                        .font(.system(size: 16))
+                        .foregroundColor(.black)
+                        .frame(width: 350, height: 100)
+                        .background(Color.white)
+                        .cornerRadius(30)
+                        .padding()
+                        .padding()
                     Button {
-                        Task {
-                            let (data, _) = try await URLSession.shared.data(from: URL(string:"https://type.fit/api/quotes")!)
-                            let decodedResponse = try? JSONDecoder().decode(InspirationQuote.self, from: data)
-                            quote = decodedResponse?.value ?? ""
-                        }
+                        fetchData()
                     } label: {
                         Text("Generate New Quote")
                             .foregroundColor(.white)
                             .frame(width: 200, height: 40)
-                            .background(.black)
+                            .background(Color.black)
                             .cornerRadius(15)
-                            .padding()
+                            .padding(.top, -20)
                     }
-                }
+                }.position(x: 200, y: 80)
+                
                 HStack(spacing: 50) {
                     NavigationLink(destination: SecondView().navigationBarBackButtonHidden(true)) {
                         Image(systemName: "house")
@@ -61,14 +65,44 @@ struct ThirdView: View {
                 .position(x: 200, y: 840)
             }
         }
-    }
-}
-struct InspirationQuote: Codable {
-    let value: String
-}
-    struct ThirdView_Previews: PreviewProvider {
-        static var previews: some View {
-            ThirdView()
+        .onAppear {
+            fetchData()
         }
     }
+    
+    func fetchData() {
+        guard let url = URL(string: "https://type.fit/api/quotes") else {
+            return
+        }
+        
+        let task = URLSession.shared.dataTask(with: url) { [self] (data, response, error) in
+            if let data = data {
+                do {
+                    let decodedResponse = try JSONDecoder().decode([InspirationQuote].self, from: data)
+                    if let randomQuote = decodedResponse.randomElement() {
+                        DispatchQueue.main.async {
+                            self.quote = randomQuote.text
+                        }
+                    }
+                } catch {
+                    print("Error decoding JSON: \(error)")
+                }
+            }
+        }
+        
+        task.resume()
+    }
+}
+
+struct InspirationQuote: Codable {
+    let text: String
+}
+
+struct ThirdView_Previews: PreviewProvider {
+    static var previews: some View {
+        ThirdView()
+    }
+}
+
+
     
