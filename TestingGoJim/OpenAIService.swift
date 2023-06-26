48357b986dfb595d58f5b0129ebf5338aa333f60
Jackie Cheng <jc51887@gmail.com>
2023-06-25 21:31:28 -0400

    OpenAIService document

--- TestingGoJim/OpenAIService.swift
+++ TestingGoJim/OpenAIService.swift
@@ -5,16 +5,42 @@
 //  Created by Jackie Cheng on 6/25/23.
 //
 
-import SwiftUI
+import Foundation
+import Alamofire
 
-struct OpenAIService: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+class OpenAIService {
+    private let endpointUrl = "https://api.openai.com/v1/chat/completions"
+    
+    func sendMessage(messages: [Message]) async -> OpenAiChatResponse? {
+        let openAIMessages = messages.map({OpenAIChatMessage(role: $0.role, content: #0.content)})
+        let body = OpenAIChatBody(model: "gpt-3.5-turbo", messages: openAIMessages)
+        let headers: HTTPHeaders = [
+            "Authorization": "Bearer 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"
+        ]
+        return try? await AF.request(endpointUrl, method: .post, parameters: body, encoder: .json, headers: headers).serializingDecodable(OpenAIChatResponse.self).value
     }
 }
 
-struct OpenAIService_Previews: PreviewProvider {
-    static var previews: some View {
-        OpenAIService()
-    }
+struct OpenAIChatBody: Encodable {
+    let model: String
+    let messages: [OpenAIChatMessage]
+}
+
+struct OpenAIChatMessage: Codable {
+    let role: SenderRole
+    let content: String
+}
+
+enum SenderRole: String, Codable {
+    case system
+    case user
+    case assistant
+}
+
+struct OpenAIChatResponse: Decodable {
+    let choices: [OpenAIChatChoice]
+}
+
+struct OpenAIChatChoice: Decodable {
+    let message: OpenAIChatMessage
 }
